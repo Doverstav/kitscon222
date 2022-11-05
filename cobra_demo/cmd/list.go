4f9126572b8c3f28c75188c88d14f3b1c092d9dd
Pontus Doverstav <doverstav@gmail.com>
2022-11-05 21:46:29 +0100

    Implement list all

--- cobra_demo/cmd/list.go
+++ cobra_demo/cmd/list.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/doverstav/kitscon222/cobra_demo/database"
 	"github.com/spf13/cobra"
@@ -22,11 +23,31 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Args: cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
-		// Should list everything
-		fmt.Println(args)
-		fmt.Println(db)
-		fmt.Println(database.GetKitscons(db))
+		kitscons := database.GetKitscons(db)
+		toPrint := ""
+
+		for _, kitscon := range kitscons {
+			toPrint += fmt.Sprintf("### %s ###\n%s\n", kitscon.Name, kitscon.Desc)
+			if len(kitscon.PresentationIds) == 0 {
+				toPrint += "\n\tNo presentations added\n"
+			}
+
+			presentations, _ := database.GetPresentations(db, kitscon.Id)
+			for _, presentation := range presentations {
+				tempString := fmt.Sprintf("\n### %s by %s ###\n%s\n%s\n%s\n",
+					presentation.PresentationTitle,
+					presentation.Presenter,
+					presentation.Desc,
+					strings.Repeat("⭐", presentation.Rating),
+					presentation.Review)
+
+				toPrint += strings.ReplaceAll(tempString, "\n", "\n\t")
+			}
+
+			toPrint += "\n"
+		}
+
+		fmt.Println(toPrint)
 	},
 }
 
